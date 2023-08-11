3a52e8b737f33be511f66adf25e3f753c612b93c
Kirill McQuillin <mcquillinkirill@gmail.com>
2023-08-11 16:04:08 -0400

    Added initial Stylling for adding TODO

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import todoItem from "./components/todoItem.js";
+import "./styles/home.css";
 
 const addTodoBtn = document.querySelector(".addItemBtn");
 
 addTodoBtn.addEventListener("click", () => {
-  console.log("add todo");
+  const userTitle = document.querySelector(".todoTitle").value;
+  const userDescription = document.querySelector(".todoDescription").value;
+  const userDate = document.querySelector(".todoDueDate").value;
+  let newTODO = new todoItem(userTitle, userDescription, userDate);
+  console.log(newTODO);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,16 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: "TODO App",
@@ -14,4 +24,12 @@ module.exports = {
       template: "src/template.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
